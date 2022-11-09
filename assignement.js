38f2b59e58718ef3c5e187f326205cc80bf032b9
Josephine Serri <josephineserri@gmail.com>
2022-11-09 15:37:28 +0100

    first exercise

---
+++ assignement.js
@@ -0,0 +1,7 @@
+let country = "Italy";
+let continent = "Europe";
+let population = "59.433.744";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ script.js
@@ -0,0 +1,6 @@
+let js = "Amazing";
+console.log(10+20+32-10);
+console.log("Jonas");
+
+let firstName = "Jonas";
+console.log(firstName);
