9f34ad5471d818b43c56f4d51b95a877ab93e05e
Josephine Serri <josephineserri@gmail.com>
2022-11-10 23:02:11 +0100

    exercise 4 Basic Operator

--- assignement.js
+++ assignement.js
@@ -1,11 +1,11 @@
 //Dichiarazione Variabile (1)
 const country = "Italy";
 const continent = "Europe";
-let population = 59433744;
+let itaPopulation = 59433744;
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(itaPopulation);
 
 //Typeof Variables (2)
 const isIsland = false;
@@ -17,3 +17,34 @@ console.log(typeof country);
 console.log(typeof language);
 
 //Variable Type (3) let, const, var
+
+//Basic Operator(4)
+itaHalfPopulation = itaPopulation / 2;
+console.log(itaHalfPopulation);
+
+itaPopulation++;
+console.log(itaPopulation);
+
+let finPopulation = 6000000;
+
+const itaFinAvrPop = itaPopulation > finPopulation;
+console.log(itaFinAvrPop);
+
+let avrCountryPop = 3300000;
+
+const avrItaPopulation = itaPopulation < avrCountryPop;
+console.log(avrItaPopulation);
+
+let description = country;
+description += ' ';
+description += 'is in ';
+description += continent;
+description += ', and its';
+description += ' ';
+description += itaPopulation;
+description += ' ';
+description += 'million people speak';
+description += ' ';
+description += language;
+
+console.log(description);
--- script.js
+++ script.js
@@ -56,6 +56,7 @@ console.log(typeof null);
 
 */
 
+/*
 //Variable Type (Let, Const, Var)
 
 //Let can change value
@@ -75,6 +76,42 @@ job = "Teacher";
 lastName = "Serri"; //Bad
 console.log(lastName);
 
+*/
+
+//BASIC OPERATOR
+/*
+//Math Operator
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+//Assignment Operator
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x++;
+x--;
+x--;//Decrease by one
+console.log(x);
+
+//Comparison Operator
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+*/
 
 
 
