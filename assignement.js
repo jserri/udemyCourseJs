04060a9de334229795db520663935d8c24a68cf9
Josephine Serri <josephineserri@gmail.com>
2022-11-10 20:46:06 +0100

    exercise 2 Data Type

--- assignement.js
+++ assignement.js
@@ -1,7 +1,15 @@
 let country = "Italy";
 let continent = "Europe";
-let population = "59.433.744";
+let population = 59433744;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- script.js
+++ script.js
@@ -1,6 +1,80 @@
+/*
 let js = "Amazing";
 console.log(10+20+32-10);
 console.log("Jonas");
 
 let firstName = "Jonas";
+
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+//Variable Name Conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+//Naming Variable Good
+let myFirstJob = "Coder";
+let myCurrentJob = "Programmer";
+
+//Naming Variable Bad
+let job1 = "Programmer";
+let job2 = "Coder";
+
+console.log(myFirstJob);
+*/
+
+/*
+//Boolean Value
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 27);
+console.log(typeof "Jonas");
+
+//Reassigned Variable
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//Undefined Variable
+let year;
+console.log(year);
+console.log(typeof year);
+
+//Assigned new Value
+year = 1989;
+console.log(typeof year);
+
+//Null Variable (console.log print Object not Null Value)
+console.log(typeof null);
+
+*/
+
+//Variable Type (Let, Const, Var)
+
+//Let can change value
+let age = 30;
+age =31
+
+//Const can't Re-assigned Value
+const birthYear = 1989;
+birthYear = 1990; //not possible
+//const job; not Possible
+
+//Var Old Variable assign
+var job = "Programmer";
+job = "Teacher";
+
+//Always Declare a Variable
+lastName = "Serri"; //Bad
+console.log(lastName);
+
+
+
+
