96b936b069f137a59a4239d177416e0e7f71e363
Josephine Serri <josephineserri@gmail.com>
2022-11-11 19:19:33 +0100

    modify descr

--- assignement.js
+++ assignement.js
@@ -1,54 +1,42 @@
-// //Dichiarazione Variabile (1)
-// const country = "Italy";
-// const continent = "Europe";
-// let itaPopulation = 59433744;
+//Dichiarazione Variabile (1)
+const country = "Italy";
+const continent = "Europe";
+let itaPopulation = 59433744;
 
-// console.log(country);
-// console.log(continent);
-// console.log(itaPopulation);
+console.log(country);
+console.log(continent);
+console.log(itaPopulation);
 
-// //Typeof Variables (2)
-// const isIsland = false;
-// const language = "Italian";
+//Typeof Variables (2)
+const isIsland = false;
+const language = "Italian";
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
 
-// //Variable Type (3) let, const, var
+//Variable Type (3) let, const, var
 
-// //Basic Operator(4)
-// itaHalfPopulation = itaPopulation / 2;
-// console.log(itaHalfPopulation);
+//Basic Operator(4)
+itaHalfPopulation = itaPopulation / 2;
+console.log(itaHalfPopulation);
 
-// itaPopulation++;
-// console.log(itaPopulation);
+itaPopulation++;
+console.log(itaPopulation);
 
-// let finPopulation = 6000000;
+let finPopulation = 6000000;
 
-// const itaFinAvrPop = itaPopulation > finPopulation;
-// console.log(itaFinAvrPop);
+const itaFinAvrPop = itaPopulation > finPopulation;
+console.log(itaFinAvrPop);
 
-// let avrCountryPop = 3300000;
+let avrCountryPop = 3300000;
 
-// const avrItaPopulation = itaPopulation < avrCountryPop;
-// console.log(avrItaPopulation);
+const avrItaPopulation = itaPopulation < avrCountryPop;
+console.log(avrItaPopulation);
 
-// let description = country;
-// description += ' ';
-// description += 'is in ';
-// description += continent;
-// description += ', and its';
-// description += ' ';
-// description += itaPopulation;
-// description += ' ';
-// description += 'million people speak';
-// description += ' ';
-// description += language;
-
-// console.log(description);
-
+let descr = country + ' ' + 'is in ' + continent + ', and its ' + itaPopulation + ' million people speak ' + language;
+console.log(descr);
 //Coding Challenge #1
 
 //Test 1
