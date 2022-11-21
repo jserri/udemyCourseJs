61b86cef9e2ba55a807151c371e0ef7378d5cfdd
Josephine Serri <josephineserri@gmail.com>
2022-11-21 01:39:44 +0100

    truthy falsy

--- script.js
+++ script.js
@@ -205,3 +205,25 @@ console.log('19' - '13' + '17'); //String (617)
 console.log('19' - '13' + 17); //Number (23)
 console.log('123' < 57); //False
 console.log(5 + 6 + '4' + 9 - 4 - 2); //Number (1143)
+
+//TRUTHY & FALSY
+//5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all :)");
+} else {
+    console.log("You should get a Job!");
+}
+
+let height = 0;
+if(height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
